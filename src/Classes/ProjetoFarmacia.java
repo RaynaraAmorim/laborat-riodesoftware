84b112fa49da2fbd0d0eba6a6516d86a0f86ae96
Samsung <Samsung@192.168.0.112>
2020-05-10 12:15:28 -0300

    Um comit

---
+++ src/Classes/ProjetoFarmacia.java
@@ -0,0 +1,15 @@
+
+package Classes;
+
+import Formularios.tlInicio;
+
+public class ProjetoFarmacia {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        tlInicio Inicio = new tlInicio();
+        Inicio.setLocationRelativeTo(null);
+        Inicio.setVisible(true);
+    }
+    
+}
